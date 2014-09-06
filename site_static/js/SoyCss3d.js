57dccee08d94655ee33023f2a63e1df6d3e87d09
Graham Reeves <graham@grahamreeves.com>
2014-09-06 12:11:34 +0100

    Added CSS3d support tester

---
+++ site_static/js/SoyCss3d.js
@@ -0,0 +1,38 @@
+
+SoyCss3d.prototype = new SoySupport('SoyCss3d');
+RegisterSupport('Css3d', new SoyCss3d() );
+
+function SoyCss3d()
+{
+	//	call super
+	SoySupport.apply( this, arguments );
+}
+
+SoyCss3d.prototype.IsSupported = function()
+{
+	//	gr: cache this?
+	var el = document.createElement('p'),
+	has3d,
+	transforms = {
+		'webkitTransform':'-webkit-transform',
+		'OTransform':'-o-transform',
+		'msTransform':'-ms-transform',
+		'MozTransform':'-moz-transform',
+		'transform':'transform'
+	};
+	
+    // Add it to the body to get the computed style.
+    document.body.insertBefore(el, null);
+	
+    for (var t in transforms) {
+        if (el.style[t] !== undefined) {
+            el.style[t] = "translate3d(1px,1px,1px)";
+            has3d = window.getComputedStyle(el).getPropertyValue(transforms[t]);
+        }
+    }
+	
+    document.body.removeChild(el);
+	
+    return (has3d !== undefined && has3d.length > 0 && has3d !== "none");
+}
+
