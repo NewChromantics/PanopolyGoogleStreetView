b9983191c3343534ac06fa1aba1a17d0098d9518
Graham Reeves <graham@grahamreeves.com>
2014-09-11 20:36:39 +0100

    Added javascript assert

--- site_static/js/soy.js
+++ site_static/js/soy.js
@@ -71,4 +71,12 @@ function ascii (a)
 	return a.charCodeAt(0);
 }
 
-
+function assert(condition, message) {
+    if (!condition) {
+        message = message || "Assertion failed";
+        if (typeof Error !== "undefined") {
+            throw new Error(message);
+        }
+        throw message; // Fallback
+    }
+}
