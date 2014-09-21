40c5070ca7c5c0195335e76ca9a1ec7b6c51c14a
Graham Reeves <graham@grahamreeves.com>
2014-09-21 13:02:45 +0100

    Added javascript string startsWith and EndsWith

--- site_static/js/soy.js
+++ site_static/js/soy.js
@@ -1,3 +1,15 @@
+if (typeof String.prototype.startsWith != 'function') {
+	String.prototype.startsWith = function (str){
+		return this.slice(0, str.length) == str;
+	};
+}
+
+if (typeof String.prototype.endsWith != 'function') {
+	String.prototype.endsWith = function (str){
+		return this.slice(-str.length) == str;
+	};
+}
+
 function removeFromArray(array, item)
 {
     while((index = array.indexOf(item)) > -1)
