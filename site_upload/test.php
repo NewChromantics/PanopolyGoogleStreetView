81bae669eb1f1c1db573169b5398d8689f2a7542
Graham Reeves <graham@grahamreeves.com>
2014-09-21 12:21:58 +0100

    Minor correction in test.php

--- site_upload/test.php
+++ site_upload/test.php
@@ -3,8 +3,9 @@
 	echo "<div><h1>Upload limit</h1>" . ini_get('upload_max_filesize') . "</div>";
 	echo "<div><h1>POST content limit</h1>" . ini_get('post_max_size') . "</div>";
 
-	define('FFMPEG_BIN', './ffmpeg' );
-	define('FFPROBE_BIN', './ffprobe' );
+	require('panopoly.php');
+	//define('FFMPEG_BIN', './ffmpeg' );
+	//define('FFPROBE_BIN', './ffprobe' );
 	
 	exec( FFMPEG_BIN . " 2>&1", $ExecOut, $ExitCode );
 	$ExecOut = join("\n", $ExecOut );
@@ -12,6 +13,6 @@
 
 	exec( FFPROBE_BIN . " 2>&1", $ExecOut, $ExitCode );
 	$ExecOut = join("\n", $ExecOut );
-	echo "<h1>exec " . FFMPEG_BIN . "</h1>[$ExitCode] $ExecOut </div>";
+	echo "<h1>exec " . FFPROBE_BIN . "</h1>[$ExitCode] $ExecOut </div>";
 ?>
 <h1>fin.</h1>
