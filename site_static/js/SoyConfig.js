c20d045e03cc43f2d341c38265ee12b4682fa6a8
Graham Reeves <graham@grahamreeves.com>
2014-09-26 22:08:56 +0100

    Moved separation value to config. Increased resolution etc for mobile...

--- site_static/js/SoyConfig.js
+++ site_static/js/SoyConfig.js
@@ -5,20 +5,21 @@ var RENDERMODE_NONE = null;
 
 function SoyConfig($RenderMode)
 {
-	this.mFov = 60;
-	
+	this.mFov = 70;
+	this.mSeperation = 0.07;
+
 	//	larger res = clipping issues
 	//	gr: trying low res for mobile to stop crashes
 	if ( IsMobile() )
 	{
 		this.mFaceResolution = 256;
-		this.mMaxResolution = 1024;
+		this.mMaxResolution = 2048;
 	}
 	else
 	{
 		//	osx chrome css clips badly at 2000px
 		this.mFaceResolution = 1500;
-		this.mMaxResolution = 2048;
+		this.mMaxResolution = 4096;
 	}
 
 	this.mRenderMode = $RenderMode;
