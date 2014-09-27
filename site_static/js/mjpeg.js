9c43e7c65f47c3d9482477c422dec25d165c61da
Graham Reeves <graham@grahamreeves.com>
2014-09-27 01:57:12 +0100

    Added maybe better, but more complex mjpeg processing, locks up safari. but chrome copes better

--- site_static/js/mjpeg.js
+++ site_static/js/mjpeg.js
@@ -20,15 +20,19 @@ function SoyMJpeg($Url,$FrameRate,$Loop,$OnUpdateJpeg,$OnError)
 	this.mOnUpdateJpeg = $OnUpdateJpeg;
 	this.mLoop = $Loop;
 	this.mCurrentFrame = 0;
+	this.mCurrentTime = 0;
+	this.mFirstRenderTime = false;
 	this.mFinishedParse = false;
+	this.mDeferTimeout = null;
+
 	
 	var ajax = new XMLHttpRequest();
 	this.mMJpegAjax = ajax;
-	//ajax.addEventListener("progress", function($Event) { $this.OnMJpegData($Event); }, false );
+	ajax.addEventListener("progress", function($Event) { if ( !$this.mDeferTimeout )	$this.OnMJpegData($Event); }, false );
 	//	ajax.addEventListener("load", function($Event){ $this.OnReply($Event); }, false);
 	//	ajax.addEventListener("error", function($Event){ $this.OnError($Event); }, false);
 	//	ajax.addEventListener("abort", function($Event){ $this.OnError($Event); }, false);
-	ajax.onreadystatechange = function($Event) { $this.OnAjaxStateChanged($Event); };
+	ajax.onreadystatechange = function($Event) { if ( !$this.mDeferTimeout )	$this.OnAjaxStateChanged($Event); };
 
 	ajax.open("GET", this.mUrl, true );
 	//ajax.setRequestHeader('Content-Type', 'multipart/form-data;');
@@ -49,17 +53,28 @@ SoyMJpeg.prototype.PopJpeg = function()
 	//	waiting for next frame
 	if ( this.mCurrentFrame >= this.mJpegs.length )
 	{
+		//	loop
 		if ( this.mFinishedParse )
-			this.mCurrentFrame = 0;
-		return false;
+			this.mCurrentFrame = this.mCurrentFrame % this.mJpegs.length;
 	}
+	if ( this.mCurrentFrame >= this.mJpegs.length )
+		return false;
+	//if ( !( this.mCurrentFrame in this.mJpegs ) )
+	//	return false;
 	
 	//	encode to datauri for html
 	var $JpegData = this.mJpegs[this.mCurrentFrame];
+	if ( typeof $JpegData == 'undefined' )
+	{
+		console.log("bad jpeg data",$JpegData);
+		return false;
+	}
+	//console.log("pop " + this.mCurrentFrame + "/" + this.mJpegs.length );
 
 	var $Blob = $JpegData;
-//	console.log("$Blob:",$Blob);
-	var $DataUrl = URL.createObjectURL( $Blob );
+//	var $DataUrl = URL.createObjectURL( $Blob );
+	var $DataUrl = $Blob;
+	this.mLastBlobUrl = $DataUrl;
 //	console.log("new jpeg" + $DataUrl );
 	
 	this.mOnUpdateJpeg( $DataUrl );
@@ -69,12 +84,35 @@ SoyMJpeg.prototype.PopJpeg = function()
 
 SoyMJpeg.prototype.UpdateJpegToElement = function()
 {
-	this.PopJpeg();
-	this.mCurrentFrame ++;
+	//	first render
+	if ( this.mFirstRenderTime === false && this.mJpegs.length > 0 )
+	{
+		console.log("first render");
+		this.mFirstRenderTime = new Date().getTime();
+		this.mCurrentFrame = -1;
+	}
+
+	if ( this.mFirstRenderTime !== false )
+	{
+		var now = new Date().getTime();
+		var dt = now - this.mFirstRenderTime;
+		this.mCurrentTime = dt;
+		
+	//	console.log("current time",this.mCurrentTime);
+		var $UpdateRateMs = 1000/parseFloat(this.mFrameRate);
+		var NewFrame = Math.floor( this.mCurrentTime / $UpdateRateMs );
+		
+		if ( NewFrame != this.mCurrentFrame )
+		{
+			this.mCurrentFrame = NewFrame;
+			this.PopJpeg();
+		}
+	}
 	
-	var $UpdateRateMs = 1000/parseFloat(this.mFrameRate);
+	//	use request animation frame to be more CPU friendly and allow sleep
 	var $this = this;
-	setTimeout( function(){ $this.UpdateJpegToElement(); }, $UpdateRateMs );
+	requestAnimationFrame( function(){ $this.UpdateJpegToElement(); } );
+//	setTimeout( function(){ $this.UpdateJpegToElement(); }, $UpdateRateMs );
 }
 
 SoyMJpeg.prototype.OnJpeg = function($JpegData)
@@ -82,7 +120,9 @@ SoyMJpeg.prototype.OnJpeg = function($JpegData)
 //	console.log("Found jpeg");
 	var $JpegDataView = new DataView($JpegData);
 	var $Blob = new Blob([$JpegDataView], {type: "image/jpeg"});
-	this.mJpegs.push( $Blob );
+	var $DataUrl = URL.createObjectURL( $Blob );
+	//delete $Blob;
+	this.mJpegs.push( $DataUrl );
 }
 
 SoyMJpeg.prototype.OnAjaxStateChanged = function($Event)
@@ -101,6 +141,12 @@ SoyMJpeg.prototype.OnFinishedDownloadingData = function()
 
 SoyMJpeg.prototype.OnMJpegData = function($Event)
 {
+	if ( this.mDeferTimeout )
+	{
+		clearTimeout( this.mDeferTimeout );
+		this.mDeferTimeout = null;
+	}
+	
 	var $Data = $Event.target.response;
 	if ( $Data == null )
 	{
@@ -131,7 +177,12 @@ SoyMJpeg.prototype.OnMJpegData = function($Event)
 	var $DataView = new DataView($Data);
 	var $HeaderView = new DataView(this.mHeader);
 	
-	for ( var $i=this.mLastReadPos;	$i<$DataLength;	$i++ )
+	//	gr: currently with defer the Nth one seems to be corrupted...
+	//	starting next data (mLastReadPos) in wrong place?
+	var $SessionJpegs = 0;
+	var $MaxSessionJpegs = 9999;
+	
+	for ( var $i=this.mLastReadPos;	$i<$DataLength && $SessionJpegs<$MaxSessionJpegs;	$i++ )
 	{
 		var $Match = true;
 		for ( var $h=0;	$Match && $h<this.mHeader.byteLength;	$h++ )
@@ -147,16 +198,29 @@ SoyMJpeg.prototype.OnMJpegData = function($Event)
 			continue;
 		}
 		
-	//	console.log("found jpeg from " + this.mLastJpegStart + " to " + $i );
+		console.log("found jpeg #" + this.mJpegs.length + " from " + this.mLastJpegStart + " to " + $i, $SessionJpegs );
 		
 		//	found next jpeg
 		var $JpegData = $Data.slice(this.mLastJpegStart,$i);
 		this.OnJpeg( $JpegData );
+		$SessionJpegs++;
 		
 		this.mLastJpegStart = $i;
 		this.mLastReadPos = this.mLastJpegStart;
 	}
 	
+	//	more to do
+	if ( $i < $DataLength )
+	{
+		var $this = this;
+		var $DeferMs = 100;
+		if ( !this.mDeferTimeout )
+		{
+			console.log("defer");
+			this.mDeferTimeout = setTimeout( function() { $this.OnMJpegData($Event); }, $DeferMs );
+		}
+		return;
+	}
 
 	if ( $Event.target.readyState == 4 )
 		this.OnFinishedDownloadingData();
