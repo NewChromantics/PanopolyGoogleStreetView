dd4a5a0583d431e3472a1cc0ac72a1f79e46b2a9
Graham Reeves <graham@grahamreeves.com>
2014-09-27 13:27:05 +0100

    Revised mjpeg parser which produces images on demand rather than at-parse

--- site_static/js/mjpeg.js
+++ site_static/js/mjpeg.js
@@ -1,9 +1,35 @@
+function SoyFrameIndex($FirstIndex,$LastIndex)
+{
+	this.mFirstIndex = $FirstIndex;
+	this.mLastIndex = $LastIndex;
+	this.mLength = $LastIndex - $FirstIndex;
+	this.mDataUrl = null;
+}
+
+SoyFrameIndex.prototype.GetDataUrl = function($ArrayBuffer)
+{
+	//	if data url already generated, return
+	if ( this.mDataUrl !== null )
+		return this.mDataUrl;
+	
+	if ( !$ArrayBuffer )
+		return null;
+	
+	var $JpegDataView = new DataView( $ArrayBuffer, this.mFirstIndex, this.mLength );
+	var $Blob = new Blob([$JpegDataView], {type: "image/jpeg"});
+	this.mDataUrl = URL.createObjectURL( $Blob );
+	console.log("new data url",this.mDataUrl);
+	//delete $Blob;
+	return this.mDataUrl;
+}
+
+
 function SoyMJpeg($Url,$FrameRate,$Loop,$OnUpdateJpeg,$OnError)
 {
 	var $this = this;
 
 	//	if an element is provided instead of OnUpdate, make a func
-	if ( typeof $OnUpdate == 'object' )
+	if ( typeof $OnUpdateJpeg == 'object' )
 	{
 		var $Element = $OnUpdateJpeg;
 		$OnUpdateJpeg = function($JpegData) { $Element.src = $JpegData; };
@@ -11,28 +37,25 @@ function SoyMJpeg($Url,$FrameRate,$Loop,$OnUpdateJpeg,$OnError)
 	
 	this.mLastJpegStart = -1;
 	this.mLastReadPos = -1;
-	this.mJpegs = new Array();
+	this.mFrames = new Array();	//	SoyFrameIndex
 	this.mJpegHeader = null;
-	this.mMJpegAjax = null;
+	this.mAjax = null;
 	this.mUrl = $Url;
 	this.mFrameRate = $FrameRate;
 	this.mOnError = $OnError;
 	this.mOnUpdateJpeg = $OnUpdateJpeg;
 	this.mLoop = $Loop;
 	this.mCurrentFrame = 0;
-	this.mCurrentTime = 0;
-	this.mFirstRenderTime = false;
 	this.mFinishedParse = false;
-	this.mDeferTimeout = null;
 
 	
 	var ajax = new XMLHttpRequest();
-	this.mMJpegAjax = ajax;
-	ajax.addEventListener("progress", function($Event) { if ( !$this.mDeferTimeout )	$this.OnMJpegData($Event); }, false );
+	this.mAjax = ajax;
+	ajax.addEventListener("progress", function($Event) { $this.OnMJpegData($Event,'progress'); }, false );
 	//	ajax.addEventListener("load", function($Event){ $this.OnReply($Event); }, false);
 	//	ajax.addEventListener("error", function($Event){ $this.OnError($Event); }, false);
 	//	ajax.addEventListener("abort", function($Event){ $this.OnError($Event); }, false);
-	ajax.onreadystatechange = function($Event) { if ( !$this.mDeferTimeout )	$this.OnAjaxStateChanged($Event); };
+	ajax.onreadystatechange = function($Event) {	$this.OnAjaxStateChanged($Event); };
 
 	ajax.open("GET", this.mUrl, true );
 	//ajax.setRequestHeader('Content-Type', 'multipart/form-data;');
@@ -47,90 +70,72 @@ function SoyMJpeg($Url,$FrameRate,$Loop,$OnUpdateJpeg,$OnError)
 
 SoyMJpeg.prototype.PopJpeg = function()
 {
+	//	no callback to execute :/
 	if ( !this.mOnUpdateJpeg )
 		return false;
 
 	//	waiting for next frame
-	if ( this.mCurrentFrame >= this.mJpegs.length )
+	if ( this.mCurrentFrame >= this.mFrames.length )
 	{
 		//	loop
 		if ( this.mFinishedParse )
-			this.mCurrentFrame = this.mCurrentFrame % this.mJpegs.length;
+			this.mCurrentFrame = this.mCurrentFrame % this.mFrames.length;
 	}
-	if ( this.mCurrentFrame >= this.mJpegs.length )
+	if ( this.mCurrentFrame >= this.mFrames.length )
 		return false;
-	//if ( !( this.mCurrentFrame in this.mJpegs ) )
-	//	return false;
+
 	
 	//	encode to datauri for html
-	var $JpegData = this.mJpegs[this.mCurrentFrame];
-	if ( typeof $JpegData == 'undefined' )
+	var $FrameIndex = this.mFrames[this.mCurrentFrame];
+	if ( typeof $FrameIndex == 'undefined' )
 	{
-		console.log("bad jpeg data",$JpegData);
+		console.log("bad frame",this.mCurrentFrame);
 		return false;
 	}
-	//console.log("pop " + this.mCurrentFrame + "/" + this.mJpegs.length );
-
-	var $Blob = $JpegData;
-//	var $DataUrl = URL.createObjectURL( $Blob );
-	var $DataUrl = $Blob;
-	this.mLastBlobUrl = $DataUrl;
-//	console.log("new jpeg" + $DataUrl );
-	
+	var $DataUrl = $FrameIndex.GetDataUrl( this.mAjax.response );
+	if ( !$DataUrl )
+		return false;
 	this.mOnUpdateJpeg( $DataUrl );
 	
 	return true;
 }
 
-SoyMJpeg.prototype.UpdateJpegToElement = function()
+SoyMJpeg.prototype.UpdateJpegToElement = function($Timestamp)
 {
-	//	first render
-	if ( this.mFirstRenderTime === false && this.mJpegs.length > 0 )
+	//	timestamp only valid on animation call
+	$Timestamp = CheckDefaultParam($Timestamp,false);
+	if ( $Timestamp )
 	{
-		console.log("first render");
-		this.mFirstRenderTime = new Date().getTime();
-		this.mCurrentFrame = -1;
-	}
-
-	if ( this.mFirstRenderTime !== false )
-	{
-		var now = new Date().getTime();
-		var dt = now - this.mFirstRenderTime;
-		this.mCurrentTime = dt;
-		
-	//	console.log("current time",this.mCurrentTime);
 		var $UpdateRateMs = 1000/parseFloat(this.mFrameRate);
-		var NewFrame = Math.floor( this.mCurrentTime / $UpdateRateMs );
-		
-		if ( NewFrame != this.mCurrentFrame )
+		var $NewFrame = Math.floor( $Timestamp / $UpdateRateMs );
+		if ( $NewFrame != this.mCurrentFrame )
 		{
-			this.mCurrentFrame = NewFrame;
+			this.mCurrentFrame = $NewFrame;
 			this.PopJpeg();
 		}
 	}
 	
-	//	use request animation frame to be more CPU friendly and allow sleep
+	//	use request animation frame to be more CPU friendly and allow tab sleep
 	var $this = this;
-	requestAnimationFrame( function(){ $this.UpdateJpegToElement(); } );
+	requestAnimationFrame( function($Timestamp){ $this.UpdateJpegToElement($Timestamp); } );
 //	setTimeout( function(){ $this.UpdateJpegToElement(); }, $UpdateRateMs );
 }
 
-SoyMJpeg.prototype.OnJpeg = function($JpegData)
+
+SoyMJpeg.prototype.AddFrame = function($FirstIndex,$LastIndex)
 {
-//	console.log("Found jpeg");
-	var $JpegDataView = new DataView($JpegData);
-	var $Blob = new Blob([$JpegDataView], {type: "image/jpeg"});
-	var $DataUrl = URL.createObjectURL( $Blob );
-	//delete $Blob;
-	this.mJpegs.push( $DataUrl );
+	this.mFrames.push( new SoyFrameIndex($FirstIndex,$LastIndex) );
+	console.log("found jpeg #" + this.mFrames.length + " from " + $FirstIndex + " to " + $LastIndex );
+	
+
 }
 
 SoyMJpeg.prototype.OnAjaxStateChanged = function($Event)
 {
 	var $ReadyState = $Event.target.readyState;
-	if ( $ReadyState >= 3 )
+	if ( $ReadyState >= 2 )
 	{
-		this.OnMJpegData( $Event );
+		this.OnMJpegData( $Event, 'OnAjaxStateChanged' );
 	}
 }
 
@@ -139,76 +144,51 @@ SoyMJpeg.prototype.OnFinishedDownloadingData = function()
 	this.mFinishedParse = true;
 }
 
-SoyMJpeg.prototype.OnMJpegData = function($Event)
+SoyMJpeg.prototype.OnMJpegData = function($Event,$Caller)
 {
-	if ( this.mDeferTimeout )
-	{
-		clearTimeout( this.mDeferTimeout );
-		this.mDeferTimeout = null;
-	}
+	var $Ajax = $Event.target;//this.mAjax;
 	
-	var $Data = $Event.target.response;
-	if ( $Data == null )
-	{
-		if ( $Event.target.readyState == 4 )
-			this.OnFinishedDownloadingData();
+	if ( !$Ajax.response )
 		return;
-	}
+
+	var $Data = new DataView( $Ajax.response );
+
+	var $Int32Size = 4;
+	var $HeaderLength = 2;
 	
-	console.log($Event);
-	console.log($Data);
-	var $DataLength = $Data.byteLength;
-	
-	console.log( typeof $Data );
-	
-	//	first case
+	//	need to read header
 	if ( this.mHeader == null )
 	{
-		//	calc header
-		if ( $DataLength < 5 )
+		//	header in 32bit chunks for speed
+		if ( $Data.byteLength < $Int32Size * $HeaderLength )
 			return;
-		this.mHeader = $Data.slice(0,10);
-		console.log("jpeg header is ", ab2str(this.mHeader), " (" + this.mHeader.byteLength + ")" );
-		this.mLastReadPos = this.mHeader.byteLength;
+		
+		this.mHeader = new Array();
+		for ( var $i=0;	$i<$HeaderLength;	$i++ )
+			this.mHeader.push( $Data.getUint32($i*$Int32Size) );
+		console.log("jpeg header is ", Uint32ToString(this.mHeader[0]), Uint32ToString(this.mHeader[1]) );
 		this.mLastJpegStart = 0;
+		this.mLastReadPos = $Int32Size;
 	}
-	
+
 	//	look for next header
-	var $DataView = new DataView($Data);
-	var $HeaderView = new DataView(this.mHeader);
-	
-	//	gr: currently with defer the Nth one seems to be corrupted...
-	//	starting next data (mLastReadPos) in wrong place?
-	var $SessionJpegs = 0;
-	var $MaxSessionJpegs = 9999;
-	
-	for ( var $i=this.mLastReadPos;	$i<$DataLength && $SessionJpegs<$MaxSessionJpegs;	$i++ )
+	for ( var $i=this.mLastReadPos;	$i<$Data.byteLength-($Int32Size*$HeaderLength);	$i++ )
 	{
 		var $Match = true;
-		for ( var $h=0;	$Match && $h<this.mHeader.byteLength;	$h++ )
-		{
-			var $dd = $DataView.getInt8($i+$h);
-			var $hd = $HeaderView.getInt8($h);
-			$Match = $Match && ($dd == $hd);
-			//	console.log( $dd + "(" + ($i+$h) + ")==" + $hd + " (" + $Match + ")" );
-		}
+		for ( var $h=0;	$h<this.mHeader.length;	$h++ )
+			$Match &= this.mHeader[$h] == $Data.getUint32($i + ($Int32Size*$h));
 		if ( !$Match )
 		{
 			this.mLastReadPos = $i;
 			continue;
 		}
 		
-		console.log("found jpeg #" + this.mJpegs.length + " from " + this.mLastJpegStart + " to " + $i, $SessionJpegs );
-		
-		//	found next jpeg
-		var $JpegData = $Data.slice(this.mLastJpegStart,$i);
-		this.OnJpeg( $JpegData );
-		$SessionJpegs++;
+		this.AddFrame( this.mLastJpegStart, $i );
 		
 		this.mLastJpegStart = $i;
 		this.mLastReadPos = this.mLastJpegStart;
 	}
-	
+	/*
 	//	more to do
 	if ( $i < $DataLength )
 	{
@@ -221,7 +201,8 @@ SoyMJpeg.prototype.OnMJpegData = function($Event)
 		}
 		return;
 	}
-
+*/
+	//	gr: check for premature end if this ready state changes WHISLT parsing?
 	if ( $Event.target.readyState == 4 )
 		this.OnFinishedDownloadingData();
 }
