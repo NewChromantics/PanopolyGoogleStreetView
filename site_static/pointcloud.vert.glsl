7757456dfce84f6db2205ae6c9bb17165faabee0
Graham Reeves <graham@grahamreeves.com>
2014-10-15 23:11:57 +0100

    Updated vertex shader from index.html's debug shader

--- site_static/pointcloud.vert.glsl
+++ site_static/pointcloud.vert.glsl
@@ -3,17 +3,21 @@
 varying vec4 vColor;
 uniform sampler2D DiffuseTexture;
 uniform sampler2D DepthTexture;
+uniform float gDepthNear;
+uniform float gDepthFar;
+uniform float gPointSize;
+uniform float gWorldYaw;
 
 //	from http://aras-p.info/blog/2009/07/30/encoding-floats-to-rgba-the-final/
 /*
-vec4 EncodeFloatRGBA( float v )
-{
-	vec4 enc = vec4(1.0, 255.0, 65025.0, 160581375.0) * v;
-	enc.x = frac(enc.x);
-	enc -= enc.yzww * vec4(1.0/255.0,1.0/255.0,1.0/255.0,0.0);
-	return enc;
-}
-*/
+ vec4 EncodeFloatRGBA( float v )
+ {
+ vec4 enc = vec4(1.0, 255.0, 65025.0, 160581375.0) * v;
+ enc.x = frac(enc.x);
+ enc -= enc.yzww * vec4(1.0/255.0,1.0/255.0,1.0/255.0,0.0);
+ return enc;
+ }
+ */
 float DecodeFloatRGBA( vec4 rgba )
 {
 	return dot( rgba, vec4(1.0, 1.0/255.0, 1.0/65025.0, 1.0/160581375.0) );
@@ -58,37 +62,56 @@ vec2 GetLatLong(float x,float y,float Width,float Height)
 	return vec2( lat, lon );
 }
 
+float lerp(float From,float To,float Time)
+{
+	return From + (To-From)*Time;
+}
 
 void main()
 {
-	float Scale = 50.0;
-	float DepthScale = 50.0;
-
-	vec2 Diffuseuv = vec2( position.x,position.y );
-	vec2 Depthuv = vec2( position.x,position.y );
+	//	debug view different angle
+	float x = position.x + gWorldYaw;
+	if ( x < 0.0 )
+		x += 1.0;
+	if ( x > 1.0 )
+		x -= 1.0;
 	
-	float x = (position.x - 0.5) * Scale;
-	float y = (position.y - 0.5) * Scale;
+	vec2 Diffuseuv = vec2( 1.0-x,position.y );
+	vec2 Depthuv = vec2( 1.0-x,position.y );
 	
 	vec4 Depth4 = texture2D( DepthTexture, Depthuv );
 	bool DepthInf = Depth4.w < 0.5;
 	Depth4.w = 0.0;
-	float Depthf = DecodeFloatRGB( Depth4 );
-	//float Depthf = 0.0;
-
-	float z = DepthScale * Depthf;
-
-
+	float Depthf = DepthInf ? 1.0 : DecodeFloatRGB( Depth4 );
+	
+	///Depthf = 1.0;
 	//	lat lon to view
 	vec3 View3 = VectorFromCoordsRad( GetLatLong(position.x, position.y,1.0,1.0) );
-	View3 *= Scale * z;
-
+	View3 = normalize(View3);
 	
-	vec4 mvPosition = modelViewMatrix * vec4( View3, 1.0 );
-
-	gl_PointSize = DepthInf ? 0.0 : 10.1;
-
-	gl_Position = projectionMatrix * mvPosition;
-
+	//	gr: have a near value so the floor bends and isnt flat...
+	float WorldDepth = lerp( gDepthNear, gDepthFar, Depthf );
+	vec3 World3 = View3 * WorldDepth;
+	
+	vec4 World4 = modelViewMatrix * vec4( World3, 1.0 );
+	
+	gl_PointSize = gPointSize;
+	
+	gl_Position = projectionMatrix * World4;
+	
 	vColor = texture2D( DiffuseTexture, Diffuseuv );
+	
+	if ( DepthInf )
+	{
+		//gl_PointSize = 0.0;
+		vColor.r *= 0.2;
+		vColor.g *= 0.2;
+		vColor.b *= 0.2;
+	}
+	else
+	{
+		//vColor.r = 1.0-Depthf;
+		//	vColor.g = 1.0-Depthf;
+		//vColor.b = 1.0-Depthf;
+	}
 }
